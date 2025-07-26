81cc042f701c18c098e36dd5d122ca2b0764a4ed
nordeim <nordeimkuahsziyp@outlook.com>
2025-07-26 19:34:58 +0800

    Create LoginForm.tsx

---
+++ src/components/auth/LoginForm.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import * as React from 'react'
+import { useRouter } from 'next/navigation'
+import { signIn } from 'next-auth/react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+import { Eye, EyeOff, Loader2 } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Checkbox } from 
